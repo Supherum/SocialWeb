975a7fcef0aaa428d0e8164db9a375736bae98ba
repiso.sapab21 <repiso.sapab21@triana.salesianos.edu>
2022-02-11 12:52:08 +0100

    init

---
+++ instaFake/src/main/java/com/repiso/sasiain/pablo/instaFake/InstaFakeApplication.java
@@ -0,0 +1,13 @@
+package com.repiso.sasiain.pablo.instaFake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaFakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstaFakeApplication.class, args);
+	}
+
+}
---
+++ instaFake/src/test/java/com/repiso/sasiain/pablo/instaFake/InstaFakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repiso.sasiain.pablo.instaFake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaFakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
